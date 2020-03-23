012b2ebca16d267a024e74013d421ca154596133
Rowen Gracious <rowengracious@gmail.com>
2020-03-23 11:17:50 -0300

    intial setup for login page

---
+++ testing-bootstrap/src/localize/fr.js
@@ -0,0 +1,20 @@
+const fr = {
+  application: {
+    email: 'Email',
+    password: 'Mot de passe',
+  },
+  login: {
+    welcome: 'Bienvenue',
+    keepLogin: 'Rester connecté',
+    signIn: 'Se Connecter',
+    createAccount: 'Créer un Compte',
+    forgotPassword: 'Mot de Passe Oublié?',
+    language: 'La Langue',
+  },
+  validation: {
+    required: 'c\'est nécessaire',
+    email: 'Adresse e-mail invalide',
+  },
+};
+
+export default fr;
---
+++ testing-padding-margins/src/localize/sp.js
@@ -0,0 +1,16 @@
+const sp = {
+  application: {
+    email: 'Email',
+    password: 'Contraseña',
+  },
+  login: {
+    welcome: 'Bienvenido',
+    keepLogin: 'Mantenme conectado',
+    signIn: 'Registrarse',
+    createAccount: 'Crear Una Cuenta',
+    forgotPassword: '¿Se te Olvidó tu Contraseña?',
+    language: 'Idioma',
+  },
+};
+
+export default sp;
